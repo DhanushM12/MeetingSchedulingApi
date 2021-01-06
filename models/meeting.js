a48d854c743a2e114f3646590225f1b0a73b07c4
DhanushM12 <dhanushmukhi@gmail.com>
2021-01-06 17:25:11 +0530

    added the meetings schema

---
+++ models/meeting.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+//meeting schema design
+const meetingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    participants: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Participants",
+        },
+      ],
+    start_time:{
+        type: String,
+      required: true,
+    },
+    end_time:{
+        type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//passing the meetingSchema instance to mongoose.model
+const Meetings = mongoose.model("Meetings", meetingSchema);
+
+//exporting the schema to be used further
+module.exports = Meetings;
